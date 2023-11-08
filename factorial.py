0aa474383041974ade1434345fc3cc8f5422f128
Corinne <CorinneKogut@gmail.com>
2023-11-08 11:53:25 -0500

    finished

--- factorial.py
+++ factorial.py
@@ -6,3 +6,11 @@ def factorial(n):
 
 
 print (factorial(5))
+
+def fact_recur(n):
+    if n == 0:
+        return 0
+    else:
+        return factorial(n-1) * n
+
+print (factorial(5))
--- fibonacci_recursive.py
+++ fibonacci_recursive.py
@@ -1,2 +1,11 @@
 def fibonacci(n):
+    if n < 0:
+        print("Need a number greater than or equal to zero.")
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
     
+print(fibonacci(7))
